0b8e0418b7647ee1353de845bd3f55d88b892fd3
lapaCaio <lapacaio2001@gmail.com>
2022-12-13 14:51:21 -0300

    UPD - main.c

--- trabalho_pratico/main.c
+++ trabalho_pratico/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct pessoa{
+    char* nome;
+    int idade;  //sendo idade menor ou igual a 20 ou maiores que 20
+    char sexo;  //sendo sexo F ou M
+    //criar um "char tipo" para ajudar na classificação
+    char** musicas[5];
+}Pessoa;
+
 int main(int argc, char** argv){
 
 
